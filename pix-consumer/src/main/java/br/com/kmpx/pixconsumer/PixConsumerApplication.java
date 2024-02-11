537e613889b5fb260e9f57ce045227dba0118126
OdirleiK <odirlei67777@gmail.com>
2024-02-11 09:06:20 -0300

    :sparkles: feat: Created kafka consumer

---
+++ pix-consumer/src/main/java/br/com/kmpx/pixconsumer/PixConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.kmpx.pixconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixConsumerApplication.class, args);
+	}
+
+}
---
+++ pix-consumer/src/test/java/br/com/kmpx/pixconsumer/PixConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kmpx.pixconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
