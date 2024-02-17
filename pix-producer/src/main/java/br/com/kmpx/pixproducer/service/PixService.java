303d29d13cd172c5951bd2cd16ee863a64b973e2
OdirleiK <odirlei67777@gmail.com>
2024-02-17 07:01:23 -0300

    :recycle: refactor: Organized code

--- pix-producer/src/main/java/br/com/kmpx/pixproducer/service/PixService.java
+++ pix-producer/src/main/java/br/com/kmpx/pixproducer/service/PixService.java
@@ -1,13 +1,8 @@
 package br.com.kmpx.pixproducer.service;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-
-import br.com.kmpx.pixproducer.config.model.Pix;
 
 @Service
 public class PixService {
@@ -15,19 +10,5 @@ public class PixService {
     @Autowired
 	KafkaTemplate<String, Object> template;
 	
-	public void sendEventsToTopic(Pix pix) {
-		try {
-            CompletableFuture<SendResult<String, Object>> future = template.send("pix-topic", pix);
-            future.whenComplete((result, ex) -> {
-                if (ex == null) {
-                    System.out.println("Sent message=[" + pix.toString() +
-                            "] with offset=[" + result.getRecordMetadata().offset() + "]");
-                } else {
-                    System.out.println("Unable to send message=[" +
-                            pix.toString() + "] due to : " + ex.getMessage());
-                }
-            });
-
-        return pixDTO;
-	}
+	
 }
